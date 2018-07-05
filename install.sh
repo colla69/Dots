bbea5c3639801c0d5f55de2c69ac46ffc20dbe09
Andrea Colarieti Tosti <colla69@gmail.com>
2018-07-05 13:20:50 +0200

    details

--- install.sh
+++ install.sh
@@ -10,12 +10,13 @@ desktop="cola-desktop"
 
 if  [ "$pcname" = "$desktop" ]
 then
-	cp -r /i3Desktop/i3 ~/.config
+	cp -r i3Desktop/i3 ~/.config
 	echo "copied /i3Desktop/i3"
 else
-	cp -r /i3Laptop/i3 ~/.config
+	cp -r i3Laptop/i3 ~/.config
 	echo "copied /i3Laptop/i3"
 fi
 
 i3-msg reload
-
+echo 
+echo done!
