32299e126ae2930ebbcf9eb38387f1b916c76d87
colla69 <colla69@gmail.com>
2019-04-23 08:03:19 +0200

    fishUpdate

---
+++ fish/conf.d/baseFunctions.fish
@@ -0,0 +1,15 @@
+
+function sandbox
+	cd ~/sandbox/
+end
+
+function remycroft
+	mycroft-start all restart
+end
+
+function uni 
+	cd ~/sandbox/UniRepo/
+end
+
+
+
---
+++ fish/conf.d/config.fish
@@ -0,0 +1,13 @@
+
+tmux
+export LC_ALL="en_US.UTF-8"
+
+function la
+	ls -la $argv
+end
+
+function ll
+	ls -l $argv
+end
+
+
