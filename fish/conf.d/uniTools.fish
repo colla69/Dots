09817b63234d71bf7477f25c2c5f9de16466f258
colla69 <colla69@gmail.com>
2019-04-24 16:32:03 +0200

    uniInfo

--- fish/conf.d/uniTools.fish
+++ fish/conf.d/uniTools.fish
@@ -4,8 +4,13 @@ function uni
 	cd ~/sandbox/UniRepo/
 end
 
+function uniCfg 
+	vim ~/.config/fish/conf.d/uniTools.fish
+end
+
 function uniInfo
-	sensible-browser "http://www.dbs.ifi.lmu.de/cms/studium_lehre/lehre_bachelor/algodat19/index.html"
+	sensible-browser --new-tab "http://www.dbs.ifi.lmu.de/cms/studium_lehre/lehre_bachelor/algodat19/index.html" --new-tab "https://www.pms.ifi.lmu.de/lehre/logik/19ss" &
+
 end
 
 
