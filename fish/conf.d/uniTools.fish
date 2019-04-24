c5d16323046085f4260fc4293e17e9f3c9e1d88e
colla69 <colla69@gmail.com>
2019-04-24 16:44:22 +0200

    test unipush

--- fish/conf.d/uniTools.fish
+++ fish/conf.d/uniTools.fish
@@ -8,6 +8,13 @@ function uniCfg
 	vim ~/.config/fish/conf.d/uniTools.fish
 end
 
+function uniPush 
+	cd -~/sandbox/UniRepo/
+	git add *
+	git commit -m $argv
+	git push ssh://git@github.com/colla69/Dots.git
+end
+
 function uniInfo
 	sensible-browser --new-tab "http://www.dbs.ifi.lmu.de/cms/studium_lehre/lehre_bachelor/algodat19/index.html" --new-tab "https://www.pms.ifi.lmu.de/lehre/logik/19ss" &
 
