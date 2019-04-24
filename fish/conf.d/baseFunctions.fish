1813c69787eb39d9fd0ba3098dde24d00ec0f8dc
colla69 <colla69@gmail.com>
2019-04-24 16:07:38 +0200

    uniInfo

--- fish/conf.d/baseFunctions.fish
+++ fish/conf.d/baseFunctions.fish
@@ -14,7 +14,7 @@ end
 function fishSave
 	fishBkpCnf
 	cd ~/sandbox/Dots/
-	git add *
+	sudo git add .
 	sudo git commit -m $argv
 	git push ssh://git@github.com/colla69/Dots.git
 end
@@ -37,10 +37,6 @@ end
 
 function dots 
 	cd ~/sandbox/Dots/
-end
-
-function uni 
-	cd ~/sandbox/UniRepo/
 end
 
 
---
+++ fish/conf.d/uniTools.fish
@@ -0,0 +1,11 @@
+
+
+function uni 
+	cd ~/sandbox/UniRepo/
+end
+
+function uniInfo
+	sensible-browser "http://www.dbs.ifi.lmu.de/cms/studium_lehre/lehre_bachelor/algodat19/index.html"
+end
+
+
