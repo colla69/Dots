20939cff07d2f3c4c9eec261fab42bf3186cc6c2
colla69 <colla69@gmail.com>
2019-07-27 14:44:50 +0200

    update

---
+++ # Create a plasma + i3 launcher script.sh
@@ -0,0 +1,14 @@
+# Create a plasma + i3 launcher script
+echo -e "#!/bin/sh\n\nKDEWM=/usr/bin/i3 startkde" > /usr/local/bin/startkde-i3
+
+sudo chown root /usr/local/bin/startkde-i3
+sudo chmod 755 /usr/local/bin/startkde-i3
+
+# Create a custom desktop sessions directory
+sudo cp -a /usr/share/xsessions /usr/local/share/xsessions
+
+# Create a new desktop session file for plasma + i3
+cp /usr/local/share/xsessions/plasma.desktop /usr/local/share/xsessions/plasma-i3.desktop
+
+sudo sed -i 's|/usr/bin/startkde|/usr/local/bin/startkde-i3|' /usr/share/xsessions/plasma-i3.desktop
+sudo sed -i '/Name.*=/ s/$/-i3/' /usr/share/xsessions/plasma-i3.desktop
