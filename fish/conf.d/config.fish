54fb48c3284d4ae97654621794ea4c268d742901
Andrea Colarieti Tosti <a.colarietitosti@googlemail.com>
2020-01-14 11:06:44 +0100

    update

--- fish/conf.d/config.fish
+++ fish/conf.d/config.fish
@@ -1,6 +1,9 @@
 
 #set -e TMUX
+
 export LC_ALL="en_US.UTF-8"
+setxkbmap -option altwin:swap_al
+
 # export PATH="/home/cola/anaconda3/bin:$PATH"
 
 function la
@@ -11,4 +14,6 @@ function ll
 	ls -lh $argv
 end
 
-
+function sandbox
+        cd ~/sandbox/
+end
